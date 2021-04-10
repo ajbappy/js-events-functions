8aa76bbbcf177a88ca9fe6eb9badccc2d4c2f0ad
ajbappy <77575985+ajbappy@users.noreply.github.com>
2021-04-10 17:26:49 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function textChange(){
+document.getElementById("p").innerHTML ="welcome";
+
+}
+
+function textChangeH2(){
+	document.getElementById("h2").innerHTML="welcome";
+}
+
+function textChangeHellow(){
+	document.getElementById("hellow").innerHTML="welcome";
+}
+
+function textAlert(){
+	document.getElementById("alert").innerHTML="welcome";
+}
+
+function textWelcome(){
+	document.getElementById("welcome").innerHTML= "welcome";
+}
+
+var d = new Date();
+d.setFullYear(2021);
+document.getElementById("demo").innerHTML = d;
+
+
+function myf(){
+	document.write("thank you")
+}
+
+function myans(){
+	document.write("I am fine")
+}
